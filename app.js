550fceafabb1a9049a5fe343db9433a7be3272a3
Akash Kumar Seth <kumarakashseth007@gmail.com>
2018-09-12 23:06:39 +0530

    Making http request to fetch data from MapQuest API using npm 'request' package.

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const request = require('request');
 const fs = require('fs');
 
-const key = JSON.parse(fs.readFileSync('keys.json')).geocodeKey;
+const key = JSON.parse(fs.readFileSync('keys.json')).mapQuestKey;
 
 let options = {
-  url: `https://maps.googleapis.com/maps/api/geocode/json?key=${key}&address=1301%20lombard%20street%20philadelphia`,
+  url: `http://www.mapquestapi.com/geocoding/v1/address?key=${key}&location=1301%20lombard%20street%20philadelphia`,
   json: true
 };
 
 function callback(error, response, body) {
   if (!error && response.statusCode === 200) {
-    console.log(`Address : ${body['results'][0]['formatted_address']}`);
-    console.log(`Latitude : ${body['results'][0]['geometry']['location']['lat']}`);
-    console.log(`Longitude : ${body['results'][0]['geometry']['location']['lng']}`);
+    console.log(response);
+    // console.log(`Address : ${body['results'][0]['formatted_address']}`);
+    console.log(`Latitude : ${body['results'][0]['locations'][0]['latLng']['lat']}`);
+    console.log(`Longitude : ${body['results'][0]['locations'][0]['latLng']['lng']}`);
   }
 }
 
